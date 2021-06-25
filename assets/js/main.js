d78a0b258ab80de37a84e62426f04a363969f945
timoshevichaleks <alti2692@gmail.com>
2021-06-25 22:05:57 +0300

    добавил карусель

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,29 @@
   });
 
 })(jQuery);
+
+const prevButton = $('#prev');
+const nextButton = $('#next');
+
+let slides = $('.slide');
+let currentSlide = 0;
+
+function goToSlide(n) {
+  $(slides[currentSlide]).toggleClass('active');
+  currentSlide = (n + slides.length) % slides.length;
+  $(slides[currentSlide]).toggleClass('active');
+}
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  goToSlide(currentSlide - 1);
+
+}
+
+let slideInterval = setInterval(nextSlide, 5000);
+
+prevButton.on('click', prevSlide);
+nextButton.on('click', nextSlide);
